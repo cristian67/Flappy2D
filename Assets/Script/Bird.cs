0d05c138048c9c5b89ca918cdb64e5c93268d543
Cristian <cristian.rock78@gmail.com>
2018-12-26 13:51:31 -0300

    Primer commit

---
+++ Assets/Script/Bird.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private bool isDead;
+    private Rigidbody2D rb2d;
+    public float upForce = 200f;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isDead) return; 
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }  
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+    }
+
+}
